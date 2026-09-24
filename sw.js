092fba9a96fa4feed06f58502177a7554556c724
jaypatel3654 <jaypatel1998@yahoo.com>
2026-09-24 06:02:30 -0400

    Refresh cache for redesigned bottom tabs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="jk-study-v11-analytics";
+const CACHE="jk-study-v12-tabs";
 const ASSETS=["./","./index.html","./app.js","./manifest.webmanifest","./icon.png","./logo.png","./photo_couple.jpg","./photo_heart.jpg","./photo_woman.jpg","./photo_man.jpg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([
