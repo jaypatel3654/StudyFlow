3487eaf5e3f0202c73afbc43579230584297bd81
jaypatel3654 <jaypatel1998@yahoo.com>
2026-09-24 03:40:25 -0400

    Refresh cache for admin analytics

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="jk-study-v10-shared";
+const CACHE="jk-study-v11-analytics";
 const ASSETS=["./","./index.html","./app.js","./manifest.webmanifest","./icon.png","./logo.png","./photo_couple.jpg","./photo_heart.jpg","./photo_woman.jpg","./photo_man.jpg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([
