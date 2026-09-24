586c162c81730a527d4c11cb11a71899dea884f6
jaypatel3654 <jaypatel1998@yahoo.com>
2026-09-24 02:15:41 -0400

    Refresh PWA cache for ENG 2211 assignments

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="jk-study-v5";
+const CACHE="jk-study-v6";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.png","./logo.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([
